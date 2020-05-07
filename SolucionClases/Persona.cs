507b721739bb8d12d6297589e81e1cc17e4a47f9
CarlosAkel <cmakel@miuandes.cl>
2020-05-07 10:01:33 -0400

    aun no terminado

---
+++ SolucionClases/Persona.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SolucionClases
+{
+    [Serializable()]
+    public class Persona : ISerializable
+    {
+        private string Nombre{ get; set; }
+        private string Apellido { get; set; }
+        private int Edad { get; set; }
+
+
+        List<string> Names = new List<string>() ;
+        List<string> LastName = new List<string>();
+        List<int> Age = new List<int>();
+        public Persona(string Nombre, string Apellido, int Edad)
+        {
+            this.Nombre = Nombre;
+            this.Apellido = Apellido;
+            this.Edad = Edad;
+        }
+        
+        public void SavePersona()
+        {
+            Names.Add(Nombre);
+            LastName.Add(Apellido);
+            Age.Add(Edad);
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Nombre", Nombre);
+            info.AddValue("Apellido", Apellido);
+            info.AddValue("Edad", Edad);
+        }
+        public Persona(SerializationInfo info, StreamingContext context)
+        {
+            Nombre = (string)info.GetValue("Nombre", typeof(string));
+            Apellido = (string)info.GetValue("Apellido", typeof(string));
+            Edad = (int)info.GetValue("Edad", typeof(int));
+
+        }
+            
+        public bool Tipe()
+        {
+            Console.WriteLine(Nombre);
+            Console.WriteLine(Apellido);
+            Console.WriteLine(Edad);
+            return true;
+
+        }
+    }
+
+}
---
+++ SolucionClases/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+namespace SolucionClases
+{
+    [Serializable()]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("Nombre");
+                string Nombre = Console.ReadLine();
+                Console.WriteLine("Apellido");
+                string Apellido = Console.ReadLine();
+                Console.WriteLine("Edad");
+                int Edad = Int32.Parse(Console.ReadLine());*/
+                Persona H = new Persona("Carlos", "Akel", 20);
+                H.SavePersona(); 
+
+             Stream stream = new FileStream("Persona.dat", FileMode.Create);
+
+            BinaryFormatter bi = new BinaryFormatter();
+            bi.Serialize(stream, H);
+            stream.Close();
+
+            H = null;
+
+
+            stream = File.Open("Persona.dat", FileMode.Open);
+
+            bi = new BinaryFormatter();
+
+            H = (Persona)bi.Deserialize(stream);
+            stream.Close();
+            H.Tipe();
+
+
+
+
+        }
+    }
+}
